8739041c3baba81ef05532024dcb0be58dcd6a8b
michaeledwards91 <michael.edwards1991@gmail.com>
2016-11-10 18:32:25 -0600

    completed coding logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,16 +16,126 @@ start over button that DOESNT REFRESh PAGE. it just starts game again by setting
 var questions = [
 	{	"q": "What color is the sky?",
 		"a": "Blue",
-	 	"possibles": ["red", "blue", "green", "yellow"]},
+	 	"possibles": ["Red", "Blue", "Green", "Yellow"],
+	 	"imgRef": "#"},
 
 	{	"q": "What color is the sky at night?",
 		"a": "Black",
-	 	"possibles": ["red", "blue", "black", "green"]}
+	 	"possibles": ["Red", "Blue", "Black", "Green"],
+	 	"imgRef": "#"}
 ]
 
 var questionIndex = 0;
-var gotCorrectAnswer;
+var correctAnswer; //will hold questions[questionIndex].a
 var answerSelection;
 var answersWrong = 0;
 var answersCorrect = 0;
 var answersTimedOut = 0;
+var secondsRemaining = 30;
+var counter;
+
+function initGame() {
+	$(".timer").hide();
+	$(".question").hide();
+	$(".answers").hide();
+	$("#playAgainButton").hide();
+	$(".possibleAnswers").html("");
+}
+function resetGame() {
+	questionIndex = 0;
+	answersCorrect = 0;
+	answersWrong = 0;
+	answersTimedOut = 0;
+	secondsRemaining = 30;
+	showQuestion();
+}
+function decrementTimer() {
+	secondsRemaining--;
+	$("#countdownNumber").html(secondsRemaining);
+	if (secondsRemaining === 0) {
+		questionTimeout();
+	}
+}
+function userCorrect() {
+	clearInterval(counter);
+	$("#questionText").html("<h2>Correct</h2>");
+	$("#possibleAnswers").html("<img src="+questions[questionIndex].imgRef+">");
+	questionIndex++;
+	answersCorrect++;
+	secondsRemaining = 30;
+	setTimeout(showQuestion, 4000);
+}
+function userWrong() {
+	clearInterval(counter);
+	$("#questionText").html("<h2>Wrong! The correct answer was '" + correctAnswer + "'</h2>");
+	$("#possibleAnswers").html("<img src="+questions[questionIndex].imgRef+">");
+	questionIndex++;
+	answersWrong++;
+	secondsRemaining = 30;
+	setTimeout(showQuestion, 4000);
+}
+//
+function questionTimeout() {
+	clearInterval(counter);
+	$("#questionText").html("<h2>Time's up! The correct answer was '" + correctAnswer + "'</h2>");
+	$("#possibleAnswers").html("<img src="+questions[questionIndex].imgRef+">");
+	questionIndex++;
+	answersTimedOut++;
+	secondsRemaining = 30;
+	setTimeout(showQuestion, 4000);
+}
+function endGame() {
+	console.log("endGame was called");
+	$(".timer").hide();
+	$("#possibleAnswers").html("");
+	$("#questionText").html("<h2>All done!</h2>");
+	$("#possibleAnswers").append("<p>Correct Answers: " + answersCorrect + "</p>");
+	$("#possibleAnswers").append("<p>Wrong Answers: " + answersWrong + "</p>");
+	$("#possibleAnswers").append("<p>Questions Timed Out: " + answersTimedOut + "</p>");
+	$("#playAgainButton").show();
+}
+
+function showQuestion() {
+	if (questions[questionIndex] === undefined) {
+		endGame();
+	} else {
+		$(".timer").show();
+		$(".question").show();
+		$(".answers").show();
+		$("#countdownNumber").html(secondsRemaining);
+		$("#possibleAnswers").html("");
+		correctAnswer = questions[questionIndex].a;
+		console.log("correct answer: " + correctAnswer);
+		counter = setInterval(decrementTimer, 1000);
+		$("#questionText").html(questions[questionIndex].q);
+		for (var i = 0; i < questions[questionIndex].possibles.length; i++) {
+			var newLi = document.createElement("li");
+			$(newLi).attr("value", questions[questionIndex].possibles[i]);
+			$(newLi).text(questions[questionIndex].possibles[i]);
+			$("#possibleAnswers").append(newLi);
+		}
+		//add click event for li's
+		$("li").on("click", function() {
+			var userChoice = this.getAttribute("value");
+			console.log(userChoice);
+
+			if (userChoice === correctAnswer) {
+				userCorrect();
+			} else {
+				userWrong();
+			}
+		})
+	}
+}
+
+$("document").ready(function () {
+	initGame();
+	$("#startButton").on("click", function () {
+		$("#startButton").hide();
+		showQuestion();
+	});
+	$("#playAgainButton").on("click", function() {
+		resetGame();
+	});
+
+})
